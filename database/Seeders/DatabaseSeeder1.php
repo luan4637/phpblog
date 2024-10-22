a1de16578e4fc11271a2a5f028ea10488fc0d1a0
Luan Pham <luan4637@gmail.com>
2024-10-22 21:22:06 +0700

    .

---
+++ database/Seeders/DatabaseSeeder1.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Core\User\UserModel;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+        $password = Hash::make('admin');
+
+        try {
+            UserModel::factory()->create([
+                'name' => 'Admin',
+                'email' => 'luan4637@gmail.com',
+                'password' => $password,
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]);
+        } catch(\Exception $e) {
+            echo 'Exist one: luan4637@gmail.com';
+        }
+    }
+}
